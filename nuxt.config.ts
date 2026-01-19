a7eb6f19fb235c35ecf3224d60d10f228d5b87aa
Wesley <wesleysouza.dev@gmail.com>
2026-01-19 01:11:49 -0300

    fix nuxt config images

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,12 @@ export default defineNuxtConfig({
   app: {
     baseURL: '/identifica-transito/'
   },
+  image: {
+    provider: 'ipx',
+    ipx: {
+      baseURL: '/identifica-transito/'
+    }
+  },
   modules: ['@nuxt/fonts', '@nuxt/image', '@primevue/nuxt-module'],
   vite: {    plugins: [      tailwindcss(),    ],  },
   css: ['./app/assets/css/main.css', './app/assets/css/styles.scss'],
